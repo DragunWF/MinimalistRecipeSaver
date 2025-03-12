24cd10e198b627a9c74f0257ba6a5ee0b4836b97
DragunDev <plarisanmarc@gmail.com>
2025-03-12 21:00:28 +0800

    Update Utils.java

--- app/src/main/java/com/example/minimalistrecipesaver/helpers/Utils.java
+++ app/src/main/java/com/example/minimalistrecipesaver/helpers/Utils.java
@@ -4,6 +4,9 @@ import android.widget.EditText;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Utils {
     public static void longToast(String message, Context context) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
@@ -15,5 +18,14 @@ public class Utils {
 
     public static String getText(EditText text) {
         return String.valueOf(text.getText());
+    }
+
+    public static ArrayList<String> convertIngredientsToList(String ingredients) {
+        String[] ingredientsArr = ingredients.split(",");
+        ArrayList<String> ingredientsList = new ArrayList<>();
+        for (String ingredient : ingredientsArr) {
+            ingredientsList.add(ingredient.trim());
+        }
+        return ingredientsList;
     }
 }
